871af81e093163b1184ba6cb7a03041b225cc71c
kapilkdev <kapilkkatel03@gmail.com>
2024-02-05 16:47:59 +0530

    changes in enum capitalisation

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,6 @@ class Member < ApplicationRecord
   has_many :opportunities_as_patient, class_name: 'Opportunity', foreign_key: 'patient_id', dependent: :destroy
   has_many :opportunities_as_doctor, class_name: 'Opportunity', foreign_key: 'doctor_id', dependent: :destroy
 
-  enum gender: { male: 1, female: 2 }
+  enum gender: { Male: 1, Female: 2 }
   enum role: { doctor: 1, patient: 2 }
 end
